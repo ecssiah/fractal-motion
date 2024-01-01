f2028037945c2ea57bea928153e5bc1b80746256
ecssiah <ecssiah@gmail.com>
2023-12-31 21:51:57 -0600

    basic frame generation working

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-escape_radius_squared = 8.0
-points = 1000000
-iterations = 1000
-dimensions = (800, 800)
-x_limits = (-2.0, 2.0)
-y_limits = (-2.0, 2.0)
+ESCAPE_RADIUS_SQUARED = 2.0 ** 2
+POINTS = 2 ** 22
+ITERATIONS = 2 ** 10
+FRAME_SIZE = 800
+DOMAIN_SIZE = 4.0
+DOMAIN_RADIUS = DOMAIN_SIZE / 2.0
 
 I = np.array([
     [ 1,  0],
@@ -18,60 +19,91 @@ R = np.array([
     [ 1,  0],
 ])
 
-B = np.array([
-    [ 0,  1],
-    [-1,  0],
-])
-
 G = np.array([
     [ 1,  0],
     [ 0, -1],
 ])
 
-def generate():
-    pixel_array = np.zeros(dimensions, dtype=int)
+B = np.array([
+    [ 0,  1],
+    [-1,  0],
+])
 
-    for _ in range(points):
+def conjugate(z):
+    z[0, 1] *= -1
+    z[1, 0] *= -1
+
+    return z
+
+
+def get_seed():
+    angle = np.random.uniform(0, 2 * np.pi)
+    radius = np.random.uniform(0, DOMAIN_RADIUS + np.finfo(float).eps)
+    
+    a = radius * np.cos(angle)
+    b = radius * np.sin(angle)
+
+    return (a, b)
+
+
+def generate():
+    pixel_array = np.zeros((FRAME_SIZE, FRAME_SIZE), dtype=int)
+
+    for point_index in range(POINTS):
+        if point_index % 20000 == 0:
+            print(f"{int(point_index / POINTS * 100)}")
+
         path = []
 
-        c_angle = np.random.uniform(0, 2 * np.pi)
-        c_radius = np.random.uniform(0, x_limits[1])
-        
-        cx = c_radius * np.cos(c_angle)
-        cy = c_radius * np.sin(c_angle)
+        a, b = get_seed()
 
-        C = cx * I + cy * B
+        C = a * I + b * B
 
         z = C
 
-        for _ in range(iterations):
-            z = z @ z + C
+        for _ in range(ITERATIONS):
+            z = np.linalg.matrix_power(conjugate(z), 2) + C
 
-            magnitude_squared = np.sum(np.square(z))
+            magnitude_squared = np.sum(np.square(z[:, 0]))
 
-            if magnitude_squared < escape_radius_squared:
+            if magnitude_squared <= ESCAPE_RADIUS_SQUARED:
                 path.append(z)
             else:
                 for point in path:
-                    cell_x = int((point[0, 0] - x_limits[0]) / (x_limits[1] - x_limits[0]) * (dimensions[0] - 1))
-                    cell_y = int((point[0, 1] - y_limits[0]) / (y_limits[1] - y_limits[0]) * (dimensions[1] - 1))
+                    x = point[0, 0]
+                    y = point[0, 1]
 
-                    if cell_x >= 0 and cell_x <= dimensions[0] - 1 and cell_y >= 0 and cell_y <= dimensions[1] - 1:
+                    cell_x = int((x + DOMAIN_RADIUS) / DOMAIN_SIZE * (FRAME_SIZE - 1))
+                    cell_y = int((y + DOMAIN_RADIUS) / DOMAIN_SIZE * (FRAME_SIZE - 1))
+
+                    in_x_bounds = cell_x >= 0 and cell_x < FRAME_SIZE
+                    in_y_bounds = cell_y >= 0 and cell_y < FRAME_SIZE
+
+                    if in_x_bounds and in_y_bounds:
+                        symmetric_cell_y = int((-y + DOMAIN_RADIUS) / DOMAIN_SIZE * (FRAME_SIZE - 1))
+                        
                         pixel_array[cell_x, cell_y] += 1
+                        pixel_array[cell_x, symmetric_cell_y] += 1
 
                 break
 
     return pixel_array
 
-pixel_array = generate()
+start_time = time.time()
 
-max_value = np.max(pixel_array)
-normalized_pixel_array = np.log1p(pixel_array) / np.log1p(max_value)
+pixel_array = generate()
+normalized_pixel_array = np.log1p(pixel_array) / np.log1p(np.max(pixel_array))
+
+end_time = time.time()
+
+elapsed_time = end_time - start_time
+
+print(f"Script execution time: {elapsed_time:.2f} seconds")
 
 plt.imshow(
     normalized_pixel_array, 
-    cmap='hot',
-    extent=[x_limits[0], x_limits[1], y_limits[0], y_limits[1]]
+    cmap='Purples_r',
+    extent=[-DOMAIN_SIZE, DOMAIN_SIZE, -DOMAIN_SIZE, DOMAIN_SIZE]
 )
 
 plt.title('Fractal Motion')
