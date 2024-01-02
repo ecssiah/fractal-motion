e64fcc6e07b055bdc26ffcfd35248559dfcb9273
ecssiah <ecssiah@gmail.com>
2024-01-02 12:56:28 -0600

    only save first frame for reference

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ import imageio
 from fm.parameter import Parameter
 
 ESCAPE_RADIUS_SQUARED = 2.0 ** 2
-POINTS = 2 ** 20
+POINTS = 2 ** 16
 ITERATIONS = 2 ** 5
 FRAME_SIZE = 800
 DOMAIN_SIZE = 4.0
@@ -59,7 +59,7 @@ def generate_frame(parameters: List[Parameter]):
     frame_array = np.zeros((FRAME_SIZE, FRAME_SIZE), dtype=np.uint8)
 
     for point_index in range(POINTS):
-        if point_index % 50000 == 0:
+        if point_index % 10000 == 0:
             print(f"{int(point_index / POINTS * 100)}%")
 
         path = []
@@ -109,29 +109,31 @@ def generate_animation():
     timestamp = datetime.now().strftime('%Y_%m_%d_%H_%M')
 
     directory = f'output/{timestamp}'
-    os.makedirs(directory + '/frames', exist_ok=True)
+    os.makedirs(directory, exist_ok=True)
+
+    frame_count = 10
 
     parameters = [
-        Parameter(coefficient=1.0, exponent=4),
-        Parameter(coefficient=0.0, exponent=2)
+        Parameter(-1.0, 6),
+        Parameter(1.0, 3)
     ]
 
+    angle = 0.0
     pixel_arrays = []
 
-    angle = 0.0
-    frame_count = 200
-
-    for frame in range(frame_count):
+    for frame_number in range(frame_count):
         parameters[0].coefficient = math.cos(angle)
         parameters[1].coefficient = math.sin(angle)
 
         angle += (2.0 * np.pi / frame_count)
 
+        print(f'Frame: {frame_number}')
+
         frame_array = generate_frame(parameters)
 
-        frame_array_normalized = frame_array
-
         max_value = np.log1p(np.max(frame_array))
+
+        frame_array_normalized = frame_array
 
         if (max_value > 0):
             frame_array_normalized = np.log1p(frame_array) / max_value
@@ -139,20 +141,28 @@ def generate_animation():
         brightness_array = (frame_array_normalized * 255).astype(np.uint8)
 
         pixel_array = np.stack([brightness_array] * 3, axis=2)
+        pixel_arrays.append(pixel_array)
 
-        frame_filename = f'{directory}/frames/frame_{frame:04d}.png'
-        imageio.imwrite(frame_filename, pixel_array)
+        if frame_number == 0:
+            frame_filename = f'{directory}/first_frame.png'
 
-        pixel_arrays.append(frame)
+            imageio.imwrite(frame_filename, pixel_array)
+
+        print('100%\n')
 
     filename = f'{directory}/fractal_motion.gif'
 
-    frames = []
+    imageio.mimsave(filename, pixel_arrays, duration=100, loop=0)
 
-    for frame in range(len(pixel_arrays)):
-        frames.append(imageio.v2.imread(f'{directory}/frames/frame_{frame:04d}.png'))
 
-    imageio.mimsave(filename, frames, duration=100, loop=0)
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def elapsed_time_to_time_string(elapsed_seconds):
+    minutes, seconds = divmod(elapsed_seconds, 60)
+
+    return f"{int(minutes)}:{int(seconds):02d}"
 
 
 def main():
@@ -164,7 +174,7 @@ def main():
 
     elapsed_time = end_time - start_time
 
-    print(f'Execution Time: {elapsed_time:.2f} s')
+    print(f'Execution Time: {elapsed_time_to_time_string(elapsed_time)}')
     
 
 if __name__ == '__main__':
