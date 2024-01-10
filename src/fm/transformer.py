fix bug with accelerating rotation

src/fm/transformer.py
@@ -8,8 +8,8 @@ from fm.generator import Generator
 
 class Transformer:
     def __init__(self) -> None:
-        self.angle = 0.0
         self.axis = np.array([0, 1, 0])
+        self.rotation = self.calculate_rotation_matrix(2.0 * np.pi / constants.FRAME_COUNT)
         
         self.generators = [Generator() for _ in range(3)]
 
@@ -20,47 +20,51 @@ class Transformer:
 
     def step(self) -> None:
         if self.generators[0].active:
+            print(self.generators[0].coefficients)
+
             self.generators[0].find_border()
             self.generators[0].calculate()
 
         if self.generators[1].active:
+            print(self.generators[1].coefficients)
+
             self.generators[1].find_border()
             self.generators[1].calculate()
 
         if self.generators[2].active:
+            print(self.generators[2].coefficients)
+
             self.generators[2].find_border()
             self.generators[2].calculate()
 
-        self.rotate(2.0 * np.pi / constants.FRAME_COUNT)
+        self.rotate()
 
 
-    def rotate(self, increment: float) -> None:
-        self.angle += increment
-
-        self.calculate_rotation_matrix()
-
+    def rotate(self) -> None:
         if self.generators[0].active:
-            self.generators[0].coefficients = self.rotation_matrix @ self.generators[0].coefficients
+            self.generators[0].coefficients = self.rotation @ self.generators[0].coefficients
 
         if self.generators[1].active:
-            self.generators[1].coefficients = self.rotation_matrix @ self.generators[1].coefficients
+            self.generators[1].coefficients = self.rotation @ self.generators[1].coefficients
 
         if self.generators[2].active:
-            self.generators[2].coefficients = self.rotation_matrix @ self.generators[2].coefficients
+            self.generators[2].coefficients = self.rotation @ self.generators[2].coefficients
 
 
-    def calculate_rotation_matrix(self) -> None:
-        c = np.cos(self.angle)
-        s = np.sin(self.angle)
+    def calculate_rotation_matrix(self, angle: float) -> np.ndarray:
+        c = np.cos(angle)
+        s = np.sin(angle)
         t = 1 - c
 
         x, y, z = self.axis
 
-        self.rotation_matrix = np.array([
+        rotation_matrix = np.array([
             [t * x * x + c,     t * x * y - s * z, t * x * z + s * y],
             [t * x * y + s * z, t * y * y + c,     t * y * z - s * x],
             [t * x * z - s * y, t * y * z + s * x, t * z * z + c    ]
         ])
+
+        return rotation_matrix
 
 
     def get_pixel_array(self) -> np.ndarray: